a3f13837b9c2d10aa460d2cd72bc90e5aee08ca1
Shiv Pandey <shivpandey.geu@gmail.com>
2026-08-15 04:22:21 +0530

    let-const-hoisting

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,6 +1,7 @@
 import {Composition} from "remotion";
 import {HelloWorld} from "./animations/javascript/hello-world/HelloWorld";
 import {Hoisting} from "./animations/javascript/hoisting/Hoisting";
+import {LetConstHoisting} from "./animations/javascript/let-const-hoisting/LetConstHoisting";
 import {SpringAnimation} from "./animations/javascript/spring-animation/SpringAnimation";
 
 export const MyComposition = () => {
@@ -26,6 +27,14 @@ export const MyComposition = () => {
         id="JS-Hoisting"
         component={Hoisting}
         durationInFrames={390}
+        fps={30}
+        width={1280}
+        height={720}
+      />
+      <Composition
+        id="JS-LetConstHoisting"
+        component={LetConstHoisting}
+        durationInFrames={540}
         fps={30}
         width={1280}
         height={720}
