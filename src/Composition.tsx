c2b684d7cc82e8d1d5313b6d74dcd4a807f9d00e
Shiv Pandey <shivpandey.geu@gmail.com>
2026-08-15 03:31:47 +0530

    updated structure

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,10 +1,5 @@
-import {
-  AbsoluteFill,
-  CalculateMetadataFunction,
-  Composition,
-  interpolate,
-  useCurrentFrame,
-} from "remotion";
+import {CalculateMetadataFunction, Composition} from "remotion";
+import {HelloWorld} from "./animations/javascript/hello-world/HelloWorld";
 
 type Props = {};
 
@@ -16,50 +11,12 @@ export const MyComposition = () => {
   return (
     <Composition
       id="MyComp"
-      component={MyComponent}
+      component={HelloWorld}
       durationInFrames={90}
       fps={30}
       width={1280}
       height={720}
       calculateMetadata={calculateMetadata}
     />
-  );
-};
-
-export const MyComponent: React.FC<Props> = () => {
-  const frame = useCurrentFrame();
-
-  return (
-    <AbsoluteFill
-      style={{
-        alignItems: "center",
-        backgroundColor: "#0f172a",
-        justifyContent: "center",
-      }}
-    >
-      <div
-        style={{
-          color: "white",
-          fontFamily: "Arial, sans-serif",
-          fontSize: 80,
-          fontWeight: 700,
-          opacity: interpolate(frame, [0, 30], [0, 1], {
-            extrapolateLeft: "clamp",
-            extrapolateRight: "clamp",
-          }),
-          translate: interpolate(
-            frame,
-            [0, 30],
-            ["-500px 0px", "0px 0px"],
-            {
-              extrapolateLeft: "clamp",
-              extrapolateRight: "clamp",
-            },
-          ),
-        }}
-      >
-        Hello JavaScript 👋
-      </div>
-    </AbsoluteFill>
   );
 };
